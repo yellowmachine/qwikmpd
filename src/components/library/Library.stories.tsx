a3dcffba3667b2213e49484e084e0e89165c4d92
Miguel Angel Alarcos Torrecillas <miguelangel.alarcos@proton.me>
2025-05-04 18:57:51 +0200

    fixing errors in stories

--- src/components/library/Library.stories.tsx
+++ src/components/library/Library.stories.tsx
@@ -20,12 +20,14 @@ export const Primary: Story = {
                 file: [
                     {
                         title: 'Stairway to Heaven',
+                        name: 'Stairway to Heaven',
                         artist: 'Led Zeppelin',
                         time: '05:00',
                         uri: 'led-zeppelin/stairway-to-heaven'
                       },
                       {
                         title: 'Enjoy the silence',
+                        name: 'Enjoy the silence',
                         artist: 'Depeche Mode',
                         time: '03:30',
                         uri: 'depeche-mode/enjoy-the-silence'
