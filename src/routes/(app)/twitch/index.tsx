556e98a867fbea9d9aed306221d4632c13ed6ccd
Miguel Angel Alarcos Torrecillas <miguelangel.alarcos@proton.me>
2025-05-17 13:26:10 +0200

    added a temporal jino twitch live

--- src/routes/(app)/twitch/index.tsx
+++ src/routes/(app)/twitch/index.tsx
@@ -12,6 +12,12 @@ export default component$(() => {
         class="mt-4 px-4 py-2 bg-brand-500 text-white rounded hover:bg-brand-600 transition duration-300">
         Escuchar Streamer temporal (pazos64)
       </button>
+      <button onClick$={$(() => {
+        playLiveTwitch("jino_destroyer");
+      })} 
+        class="mt-4 px-4 py-2 bg-brand-500 text-white rounded hover:bg-brand-600 transition duration-300">
+        JiNo_DesTroYeR
+      </button>
     </div>
   );
 });
